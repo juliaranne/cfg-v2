a22e922f87f1f6308c937811de3589ec7fa261ef
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-09 17:55:10 +0100

    Initial chromedriver use

--- e2e_testing/src/test/java/cucumber/stepDefs/LoginSteps.java
+++ e2e_testing/src/test/java/cucumber/stepDefs/LoginSteps.java
@@ -3,11 +3,23 @@ package cucumber.stepDefs;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginSteps {
+// todo have chrome driver config in a spring config file
+    private WebDriver driver;
 
     @Given("I have an account for {string} with password {string}")
     public void iHaveAnAccountForWithPassword(String username, String password) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "C:/Users/baron/Code/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+
+        // Initialize the Chrome WebDriver
+        driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
         Assertions.assertEquals(1, 1);
 //        Todo in next PR: implementation
     }
--- e2e_testing/src/test/java/cucumber/stepDefs/WeeklyJournalSteps.java
+++ e2e_testing/src/test/java/cucumber/stepDefs/WeeklyJournalSteps.java
@@ -3,12 +3,17 @@ package cucumber.stepDefs;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WeeklyJournalSteps {
+    private WebDriver driver;
 
     @Then("^I can see a daily breakdown of my workout week in the journal$")
     public void iCanSeeADailyBreakdownOfMyWorkoutWeekInTheJournal() {
-        Assertions.assertEquals(1, 1);
+        assertEquals(1, 1);
 //        Todo in next PR: implementation
         // add the expected text
         // using selenium, navigate to the weekly journal button/area the info lives
@@ -17,14 +22,26 @@ public class WeeklyJournalSteps {
 
     @And("^I can see the full details of each workout$")
     public void iCanSeeTheFullDetailsOfEachWorkout() {
-        Assertions.assertEquals(1, 1);
+        assertEquals(1, 1);
 //        Todo in next PR: implementation
         //        at the moment, this just shows the type & duration, not the details added when tracking new exercise
     }
 
     @Then("I can see my workout in the journal")
     public void iCanSeeMyWorkoutInTheJournal() {
-        Assertions.assertEquals(1, 1);
+        driver = new ChromeDriver();
+
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+
+        assertEquals("1", 2);
+        assertEquals("Gogle", actualTitle);
+
+        // Close the browser
+        driver.quit();
+
+
+//        assertEquals(1, 1);
 //        Todo in next PR: implementation
     }
 }
