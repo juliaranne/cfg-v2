73a98b362e7006288d8990e651ea56aa33607bfb
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-13 19:00:11 +0100

    moving wait to constructor of webactions

--- e2e_testing/src/test/java/cucumber/webHelpers/WebActions.java
+++ e2e_testing/src/test/java/cucumber/webHelpers/WebActions.java
@@ -1,6 +1,5 @@
 package cucumber.webHelpers;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,98 +14,74 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
-@RequiredArgsConstructor
-public class WebActions { // todo remove anything unused
+public class WebActions {
 
     private final WebDriver chromeDriver;
+    private WebDriverWait wait;
+
+    public WebActions(WebDriver chromeDriver) {
+        this.chromeDriver = chromeDriver;
+        this.wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+    }
 
     public void clickOnButtonByLinkText(String button) {
-        WebDriverWait wait = wait(chromeDriver);
-        wait.until(ExpectedConditions.elementToBeClickable(By.linkText(button))).click();
+        clickElement(By.linkText(button));
     }
 
     public void clickOnButtonByPath(String path) {
-        chromeDriver.findElement(By.xpath(path)).click();
+        clickElement(By.xpath(path));
     }
 
     public void clickSubmit() {
-        WebDriverWait wait = wait(chromeDriver);
-        try {
-            WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit' and contains(text(), 'Save activity')]")));
-
-            saveButton.click();
-
-            System.out.println("Save activity button clicked successfully.");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        clickElement(By.xpath("//button[@type='submit' and contains(text(), 'Save activity')]"), "Save activity button");
     }
 
-    public void populateField(String field, String contents) {
-        WebDriverWait wait = wait(chromeDriver);
-        WebElement usernameField = wait.until(ExpectedConditions.elementToBeClickable(By.id(field)));
-        usernameField.sendKeys(contents);
-
+    public void populateField(String fieldId, String contents) {
+        WebElement field = wait.until(ExpectedConditions.elementToBeClickable(By.id(fieldId)));
+        field.sendKeys(contents);
     }
 
     public void assertMessageAppears(String contents) {
-        WebDriverWait wait = wait(chromeDriver);
-
         String successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format("//p[contains(text(),'%s')]", contents)))).getText();
-
-        assertTrue(successMessage.equals(contents), String.format("Actual message contents: %s, expected: %s", successMessage, contents));
-
+        assertEquals(contents, successMessage, String.format("Actual message: %s, expected: %s", successMessage, contents));
     }
 
     public void assertHeaderPresent(String headerType, String contents) {
-        WebDriverWait wait = wait(chromeDriver);
-
-        try {
-            WebElement header = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format("//%s[contains(text(), '%s')]", headerType, contents))));
-
-            String headerText = header.getText();
-            assertEquals(contents, headerText, String.format("Header does not contain: %s", contents));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        WebElement header = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format("//%s[contains(text(), '%s')]", headerType, contents))));
+        assertEquals(contents, header.getText(), String.format("Header does not contain: %s", contents));
     }
 
-    public void clickOnRunningButton() { // todo maybe split by webpage here
-        WebDriverWait wait = wait(chromeDriver);
-
-        WebElement runningButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("runningButton")));
-
-        runningButton.click();
-    } // todo have this as an enum of different types // maybe have in separate classes for the different webpages
+    public void clickOnRunningButton() {
+        clickElement(By.id("runningButton"), "Running Button");
+    }
 
     public void assertLogoutButtonVisible() {
-        WebDriverWait wait = wait(chromeDriver);
         WebElement logoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Logout']")));
         assertTrue(logoutButton.isDisplayed(), "Logout button is not visible");
-
     }
 
     public void assertExerciseRecordIsPresent(String expectedRecord) {
-
         WebElement exerciseList = chromeDriver.findElement(By.tagName("ul"));
-
         List<WebElement> exercises = exerciseList.findElements(By.tagName("li"));
 
-        boolean recordFound = false;
-
-        for (WebElement exercise : exercises) {
-            if (exercise.getText().equals(expectedRecord)) {
-                recordFound = true;
-                break;
-            }
-        }
+        boolean recordFound = exercises.stream().anyMatch(exercise -> exercise.getText().equals(expectedRecord));
 
         assertTrue(recordFound, String.format("%s record wasn't found", expectedRecord));
-
     }
 
-    private WebDriverWait wait(WebDriver chromeDriver) {
-        return new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
-    } // can this be turned into a variable for the class instead
+    private void clickElement(By locator) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
+    private void clickElement(By locator, String elementDescription) {
+        try {
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            element.click();
+            log.info("{} clicked successfully.", elementDescription);
+        } catch (Exception e) {
+            log.error("Failed to click on {}", elementDescription, e);
+            throw e;
+        }
+    }
 }
