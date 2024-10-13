a0fa4f881d5e4f205302c9ebc3a57df8a82c1f56
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-13 19:33:39 +0100

    grabbing config without spring

--- e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
+++ e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
@@ -2,16 +2,32 @@ package cucumber.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
 
 public class WebDriverManager {
     private static WebDriver driver;
-    private static String baseUrl = "http://localhost";
+    private static Config config;
+
+    // Static block to load the configuration at the time of class loading
+    static {
+        try (InputStream in = WebDriverManager.class.getClassLoader().getResourceAsStream("application.yaml")) {
+            if (in == null) {
+                throw new RuntimeException("application.yaml file not found in resources");
+            }
+            Yaml yaml = new Yaml();
+            config = yaml.loadAs(in, Config.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading configuration", e);
+        }
+    }
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", "C:/Users/baron/Code/chromedriver-win64/chromedriver-win64/chromedriver.exe"); // Todo get from app.yml or something
+            System.setProperty("webdriver.chrome.driver", config.getWebdriver().getChromeDriverPath());
             driver = new ChromeDriver();
-            driver.get(baseUrl);
+            driver.get(config.getWebdriver().getBaseUrl());
         }
         return driver;
     }
