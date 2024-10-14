f58fa4aae31945b3261f1b2a20bc9525c190d04e
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-14 14:52:51 +0100

    await for consistency

--- e2e_testing/src/test/java/cucumber/webHelpers/WeeklyJournalActions.java
+++ e2e_testing/src/test/java/cucumber/webHelpers/WeeklyJournalActions.java
@@ -3,6 +3,7 @@ package cucumber.webHelpers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class WeeklyJournalActions extends WebActions {
     }
 
     public void assertExerciseRecordIsPresent(String expectedRecord) {
-        WebElement exerciseList = chromeDriver.findElement(By.tagName("ul"));
+        WebElement exerciseList = wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("ul")));
         List<WebElement> exercises = exerciseList.findElements(By.tagName("li"));
 
         boolean recordFound = exercises.stream().anyMatch(exercise -> exercise.getText().equals(expectedRecord));
