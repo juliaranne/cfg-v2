bc1d08eb8a4da0659e72923d1edc61a8b26f200e
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-19 13:54:36 +0100

    note

--- analytics_v2/app.py
+++ analytics_v2/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-# This is a template/basic app for the purpose of testing the setup in docker. But, will be removed and replaced
+# This is a template/basic app for the purpose of testing the setup in docker. But, will be removed and replaced in scrum-40
 
 @app.route('/')
 def home():
