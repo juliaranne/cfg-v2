9c89b9ad30a7b303e3b38be40b3319523921930a
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-19 13:32:26 +0100

    initial app with home endpoint

---
+++ analytics_v2/README.md
@@ -0,0 +1,6 @@
+# Analytics V2 
+A rewrite of the existing analytics service. 
+
+## How to run: 
+* This app will be running on 5051, so take note to specify this during development 
+[//]: # (Todo add these instructions  )
---
+++ analytics_v2/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+# This is a template/basic app for the purpose of testing the setup in docker. But, will be removed and replaced
+
+@app.route('/')
+def home():
+    return "Welcome to analytics V2"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5051)
