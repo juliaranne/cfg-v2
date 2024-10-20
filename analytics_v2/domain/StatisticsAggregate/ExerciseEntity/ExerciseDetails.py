b3aba53aecda8085fd4f09b98ff285097bfbdc82
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-20 11:16:01 +0100

    adding enum

--- analytics_v2/domain/StatisticsAggregate/ExerciseEntity/ExerciseDetails.py
+++ analytics_v2/domain/StatisticsAggregate/ExerciseEntity/ExerciseDetails.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
+from enum import Enum
+
+ExerciseType = Enum('Running', 'Cycling', 'Swimming', 'Gym', 'Other')
 
 
 @dataclass(frozen=True)
 class ExerciseDetails:
     date: str
     duration: int
-    exercise_type: str  # todo, make enum
+    exercise_type: ExerciseType
     description: str
