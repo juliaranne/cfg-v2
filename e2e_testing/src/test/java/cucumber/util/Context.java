ecbfb34b3593b61d68b427b7ce88ffa02d9df540
AdelaideBaron <baronadelaide@hotmail.com>
2024-10-25 15:31:07 +0100

    add description

--- e2e_testing/src/test/java/cucumber/util/Context.java
+++ e2e_testing/src/test/java/cucumber/util/Context.java
@@ -16,6 +16,7 @@ public class Context {
 
     public static String KEY_EXERCISE_TYPE = "exercise_type";
     public static String KEY_EXERCISE_DURATION = "exercise_duration";
+    public static String KEY_EXERCISE_DESCRIPTION = "exercise_description";
     public static String KEY_WEB_ACTIONS = "web_actions";
     public static String KEY_CHROME_DRIVER = "chrome_driver";
 
