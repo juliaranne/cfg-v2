0d19e75e2a91721d07aad612801b5bd13e040ac8
AdelaideBaron <baronadelaide@hotmail.com>
2024-11-15 20:36:22 +0000

    changing access to chrome driver

--- e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
+++ e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
@@ -11,45 +11,46 @@ public class WebDriverManager {
     private static WebDriver driver;
     private static Config config;
 
-    // Todo use springboot to load the config
-    static {
-        try (InputStream in = WebDriverManager.class.getClassLoader().getResourceAsStream("application.yaml")) {
-            if (in == null) {
-                throw new RuntimeException("application.yaml file not found in resources");
-            }
-            Yaml yaml = new Yaml();
-            config = yaml.loadAs(in, Config.class);
-
-            String chromeDriverPath = resolveEnvVars(config.getWebdriver().getChromeDriverPath());
-            config.getWebdriver().setChromeDriverPath(chromeDriverPath);
-
-        } catch (Exception e) {
-            throw new RuntimeException("Error loading configuration", e);
-        }
-    }
+//    // Todo use springboot to load the config
+//    static {
+//        try (InputStream in = WebDriverManager.class.getClassLoader().getResourceAsStream("application.yaml")) {
+//            if (in == null) {
+//                throw new RuntimeException("application.yaml file not found in resources");
+//            }
+//            Yaml yaml = new Yaml();
+//            config = yaml.loadAs(in, Config.class);
+//
+//            String chromeDriverPath = resolveEnvVars(config.getWebdriver().getChromeDriverPath());
+//            config.getWebdriver().setChromeDriverPath(chromeDriverPath);
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException("Error loading configuration", e);
+//        }
+//    }
 
     public static WebDriver getDriver() {
-        if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", config.getWebdriver().getChromeDriverPath());
-            driver = new ChromeDriver();
-            driver.get(config.getWebdriver().getBaseUrl());
-        }
-        return driver;
+        io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+        System.out.println("ChromeDriver version: " + io.github.bonigarcia.wdm.WebDriverManager.chromedriver().getDownloadedDriverVersion());
+
+
+        return new ChromeDriver();
     }
 
     public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
+//        if (driver != null) {
+//            driver.quit();
+//            driver = null;
+//        }
+        System.out.println("hi");
+        // how to update this?
     }
 
-    private static String resolveEnvVars(String path) {
-        if (path != null && path.contains("${")) {
-            for (Map.Entry<String, String> env : System.getenv().entrySet()) {
-                path = path.replace("${" + env.getKey() + "}", env.getValue());
-            }
-        }
-        return path;
-    }
+//    private static String resolveEnvVars(String path) {
+//        if (path != null && path.contains("${")) {
+//            for (Map.Entry<String, String> env : System.getenv().entrySet()) {
+//                path = path.replace("${" + env.getKey() + "}", env.getValue());
+//            }
+//        }
+//        return path;
+//    }
 }
