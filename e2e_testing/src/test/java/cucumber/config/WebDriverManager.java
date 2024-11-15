0ef2ed87844bd045e9a7e2ac1d8308b004608ea6
AdelaideBaron <baronadelaide@hotmail.com>
2024-11-15 22:01:18 +0000

    run in headless mode

--- e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
+++ e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
@@ -1,7 +1,8 @@
-package cucumber.config;
+package cucumber.config; // move?
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverManager {
 
@@ -11,7 +12,14 @@ public class WebDriverManager {
         if (driver == null) {
             io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
 
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless"); // Run in headless mode
+            options.addArguments("--no-sandbox"); // Disable sandboxing
+            options.addArguments("--disable-dev-shm-usage"); // Disable /dev/shm usage
+            options.addArguments("--remote-debugging-port=9222"); // Necessary for debugging in CI environments
+
+            // Initialize the driver with the Chrome options
+            driver = new ChromeDriver(options);
 
             String baseUrl = "http://localhost"; // move this
             driver.get(baseUrl);
