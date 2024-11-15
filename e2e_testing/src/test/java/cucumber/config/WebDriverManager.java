7274c541f22d5b4ab6cbf964984f3615bc36d974
AdelaideBaron <baronadelaide@hotmail.com>
2024-11-15 20:45:52 +0000

    using new chrome driver rather than local path

--- e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
+++ e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
@@ -2,55 +2,29 @@ package cucumber.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.InputStream;
-import java.util.Map;
 
 public class WebDriverManager {
-    private static WebDriver driver;
-    private static Config config;
 
-//    // Todo use springboot to load the config
-//    static {
-//        try (InputStream in = WebDriverManager.class.getClassLoader().getResourceAsStream("application.yaml")) {
-//            if (in == null) {
-//                throw new RuntimeException("application.yaml file not found in resources");
-//            }
-//            Yaml yaml = new Yaml();
-//            config = yaml.loadAs(in, Config.class);
-//
-//            String chromeDriverPath = resolveEnvVars(config.getWebdriver().getChromeDriverPath());
-//            config.getWebdriver().setChromeDriverPath(chromeDriverPath);
-//
-//        } catch (Exception e) {
-//            throw new RuntimeException("Error loading configuration", e);
-//        }
-//    }
+    private static WebDriver driver;
 
     public static WebDriver getDriver() {
-        io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
-        System.out.println("ChromeDriver version: " + io.github.bonigarcia.wdm.WebDriverManager.chromedriver().getDownloadedDriverVersion());
+        if (driver == null) {
+            io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
 
+            driver = new ChromeDriver();
 
-        return new ChromeDriver();
+            String baseUrl = "http://localhost"; // move this
+            driver.get(baseUrl);
+        }
+
+        return driver;
     }
 
     public static void closeDriver() {
-//        if (driver != null) {
-//            driver.quit();
-//            driver = null;
-//        }
-        System.out.println("hi");
-        // how to update this?
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
-
-//    private static String resolveEnvVars(String path) {
-//        if (path != null && path.contains("${")) {
-//            for (Map.Entry<String, String> env : System.getenv().entrySet()) {
-//                path = path.replace("${" + env.getKey() + "}", env.getValue());
-//            }
-//        }
-//        return path;
-//    }
 }
