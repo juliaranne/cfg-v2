a2e9a4e81a8b43ab3632ce4b2422ec54fa76aeee
AdelaideBaron <baronadelaide@hotmail.com>
2024-11-16 14:42:27 +0000

    trialing a report for cucumber

--- e2e_testing/src/test/java/cucumber/CucumberTest.java
+++ e2e_testing/src/test/java/cucumber/CucumberTest.java
@@ -3,9 +3,13 @@ package cucumber;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.CucumberOptions;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("cucumber")
+@CucumberOptions(
+        plugin = {"pretty", "json:target/cucumber-reports/cucumber-report.json"}
+)
 public class CucumberTest {
 }
