27eef1f3da130593eeec368f3865a8920df09a24
AdelaideBaron <baronadelaide@hotmail.com>
2024-12-15 18:45:34 +0000

    getting sentiment

---
+++ ai_service/SentimentAnalyser.py
@@ -0,0 +1,21 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+import spacy
+
+# Download NLTK resources
+nltk.download('punkt_tab')
+nltk.download('vader_lexicon')
+
+# Load the English NLP model from spacy
+nlp = spacy.load('en_core_web_sm')
+
+
+def analyze_description(sentence):
+    sia = SentimentIntensityAnalyzer()
+    sentiment_score = sia.polarity_scores(sentence)
+    if sentiment_score['compound'] >= 0.05:
+        return 'Positive'
+    elif sentiment_score['compound'] <= -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
--- ai_service/app.py
+++ ai_service/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, jsonify, request
+from SentimentAnalyser import analyze_description
 
 app = Flask(__name__)
-# This is a template/basic app for the purpose of testing the setup in docker. But, will be removed and replaced in scrum-40
 
 @app.route('/')
 def home():
     return "Welcome to the AI service"
+
+
+@app.route('/get_sentiment_message', methods=['POST'])
+def get_sentiment_message():
+    description = request.json
+    sentiment = analyze_description(description["sentence"])
+    return jsonify({"sentiment": sentiment})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5052)
