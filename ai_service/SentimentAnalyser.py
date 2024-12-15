2a3d382eddb0b737ba8522c4176a90331db05f64
AdelaideBaron <baronadelaide@hotmail.com>
2024-12-15 19:55:53 +0000

    comment

--- ai_service/SentimentAnalyser.py
+++ ai_service/SentimentAnalyser.py
@@ -10,7 +10,7 @@ nltk.download('vader_lexicon')
 # Load the English NLP model from spacy
 nlp = spacy.load('en_core_web_sm')
 
-generator = pipeline("text-generation", model="gpt2")
+# generator = pipeline("text-generation", model="gpt2")
 
 
 def analyze_description(sentence):
@@ -31,20 +31,21 @@ def get_motivational_message(sentiment):
     else:
         return "Don't lose your spark!"
 
-def generate_motivational_message(sentiment):
-    if sentiment == 'Positive':
-        prompt = "Generate a short motivational message to congratulate someone who is happy with their workout. For example: 'Great job on the workout! Keep up the amazing work!'"
-    elif sentiment == 'Negative':
-        prompt = "Generate a short motivational message to encourage someone who is feeling down about their workout. For example: 'Don't give up! Every workout gets you closer to your goals. You've got this!'"
-    else:  # Neutral
-        prompt = "Generate a short motivational message to keep someone motivated who is feeling neutral about their workout. For example: 'Good job today! Stay consistent, and you'll see great results!'"
-
-    # Generate the message
-    generated_text = generator(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
-
-    # Remove the prompt from the generated text
-    if generated_text.startswith(prompt):
-        generated_text = generated_text[len(prompt):].strip()
-
-    return generated_text
+# Todo revisit the below due to inacurrate responses, needs  the prompt finetuning.
+# def generate_motivational_message(sentiment):
+#     if sentiment == 'Positive':
+#         prompt = "Generate a short motivational message to congratulate someone who is happy with their workout. For example: 'Great job on the workout! Keep up the amazing work!'"
+#     elif sentiment == 'Negative':
+#         prompt = "Generate a short motivational message to encourage someone who is feeling down about their workout. For example: 'Don't give up! Every workout gets you closer to your goals. You've got this!'"
+#     else:  # Neutral
+#         prompt = "Generate a short motivational message to keep someone motivated who is feeling neutral about their workout. For example: 'Good job today! Stay consistent, and you'll see great results!'"
+#
+#     # Generate the message
+#     generated_text = generator(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
+#
+#     # Remove the prompt from the generated text
+#     if generated_text.startswith(prompt):
+#         generated_text = generated_text[len(prompt):].strip()
+#
+#     return generated_text
 
