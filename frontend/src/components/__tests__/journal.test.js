50cd6c5064cf263f93687572f60e161d1e9bba86
julia-anne <julia.robinson@spareroom.co.uk>
2024-12-17 22:45:29 +0000

    journal test start

--- frontend/src/components/__tests__/journal.test.js
+++ frontend/src/components/__tests__/journal.test.js
@@ -1,7 +1,34 @@
-import {render, screen, fireEvent, waitFor} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import Journal from '../journal';
+import useFetch from '../../hooks/useFetch.js';
 
-test('renders journal', () => {
+jest.mock('../../hooks/useFetch', () => ({
+  __esModule: true,
+  default: () => ({
+    data: {
+      weekly_stats: {
+        results: {
+          exercises: [
+            {
+              description: "lksjfaldskfjalsdf ja;lsdfjk ;",
+              exerciseType: "Running",
+              totalDuration: 3
+            },
+            {
+              description: "ldkjgaldjkga",
+              exerciseType: "Gym",
+              totalDuration: 2
+            },
+          ]
+        }
+      }
+    }
+})
+}));
+
+
+test('should display list of exercises', () => {
   render(<Journal />)
-  
+
+  expect(screen.getByText('Running - 3 minutes')).toBeInTheDocument();
 })
