e54b79dd5e2084351e319f30d029f3b1c8913b14
AdelaideBaron <baronadelaide@hotmail.com>
2024-12-17 17:52:43 +0000

    remove unused code

--- ai_service/SentimentAnalyser.py
+++ ai_service/SentimentAnalyser.py
@@ -1,7 +1,6 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import spacy
-# from transformers import pipeline
 
 # Download NLTK resources
 nltk.download('punkt_tab')
@@ -9,9 +8,6 @@ nltk.download('vader_lexicon')
 
 # Load the English NLP model from spacy
 nlp = spacy.load('en_core_web_sm')
-
-# generator = pipeline("text-generation", model="gpt2")
-
 
 def analyze_description(sentence):
     sia = SentimentIntensityAnalyzer()
@@ -30,22 +26,3 @@ def get_motivational_message(sentiment):
         return "Next time will be better!"
     else:
         return "Don't lose your spark!"
-
-# Todo revisit the below due to inacurrate responses, needs  the prompt finetuning.
-# def generate_motivational_message(sentiment):
-#     if sentiment == 'Positive':
-#         prompt = "Generate a short motivational message to congratulate someone who is happy with their workout. For example: 'Great job on the workout! Keep up the amazing work!'"
-#     elif sentiment == 'Negative':
-#         prompt = "Generate a short motivational message to encourage someone who is feeling down about their workout. For example: 'Don't give up! Every workout gets you closer to your goals. You've got this!'"
-#     else:  # Neutral
-#         prompt = "Generate a short motivational message to keep someone motivated who is feeling neutral about their workout. For example: 'Good job today! Stay consistent, and you'll see great results!'"
-#
-#     # Generate the message
-#     generated_text = generator(prompt, max_length=100, num_return_sequences=1)[0]['generated_text']
-#
-#     # Remove the prompt from the generated text
-#     if generated_text.startswith(prompt):
-#         generated_text = generated_text[len(prompt):].strip()
-#
-#     return generated_text
-
