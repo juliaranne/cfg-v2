9ba9feebae945ce48e355b92705d6783c001463e
AdelaideBaron <baronadelaide@hotmail.com>
2024-12-17 19:43:14 +0000

    upgrade java

--- authservice/src/main/java/com/authservice/auth/model/User.java
+++ authservice/src/main/java/com/authservice/auth/model/User.java
@@ -19,15 +19,6 @@ public class User {
         this.password = password;
     }
 
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getUsername() {
         return username;
     }
