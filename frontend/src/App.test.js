da730f05b7adc2543ddf3d15d1a4b2179434d9ee
julia-anne <julia.robinson@spareroom.co.uk>
2024-12-18 20:36:16 +0000

    login or signup user

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,61 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
+import axios from 'axios';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('axios');
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+test('should render fitness app login page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/MLA Fitness App/i);
-  expect(linkElement).toBeInTheDocument();
+  const siteTitle = screen.getByText(/MLA Fitness App/i);
+  expect(siteTitle).toBeInTheDocument();
+});
+
+test('should log existing user in', async () => {
+  axios.post.mockResolvedValue({
+    data: 'User authenticated',
+    status: 200
+  })
+
+  render(<App />);
+
+  fireEvent.change(screen.getByLabelText('Username'), {
+    target: { value: 'cfg_student' },
+  });
+  fireEvent.change(screen.getByLabelText('Password'), {
+    target: { value: '12345678' },
+  });
+  
+  fireEvent.click(screen.getByText('Login'));
+  
+  await waitFor(() => {
+    expect(screen.getByText('Track exercise')).toBeInTheDocument();
+  })
+});
+
+test('should sign in new user', async () => {
+  axios.post.mockResolvedValue({
+    data: "User registered successfully!",
+    status: 200
+  })
+
+  render(<App />);
+
+  fireEvent.click(screen.getByText('Sign up'));
+  expect(screen.getByText('Already have an account?')).toBeInTheDocument()
+
+  fireEvent.change(screen.getByLabelText('Username'), {
+    target: { value: 'new_cfg_student' },
+  });
+  fireEvent.change(screen.getByLabelText('Password'), {
+    target: { value: '12345678' },
+  });
+  
+  fireEvent.click(screen.getByText('Signup'));
+  
+  await waitFor(() => {
+    expect(screen.getByText('Track exercise')).toBeInTheDocument();
+  })
 });
