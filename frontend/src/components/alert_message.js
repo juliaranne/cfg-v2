e315180c650c60656b270ccec96c5d864c0fd2d7
julia-anne <julia.robinson@spareroom.co.uk>
2024-12-24 20:38:24 +0100

    prettier formatting

--- frontend/src/components/alert_message.js
+++ frontend/src/components/alert_message.js
@@ -1,32 +1,40 @@
-import { useEffect, useState, useCallback, useRef } from 'react';
-import CloseIcon from '@material-ui/icons/Close';
+import { useEffect, useState, useCallback, useRef } from "react";
+import CloseIcon from "@material-ui/icons/Close";
 
-const AlertMessage = ({message, handleUnmount}) => {
-    const [show, setShow] = useState(false);
-    const closeBtnRef = useRef();
-    const alertElRef = useRef();
+const AlertMessage = ({ message, handleUnmount }) => {
+  const [show, setShow] = useState(false);
+  const closeBtnRef = useRef();
+  const alertElRef = useRef();
 
-    const dismissAlert = useCallback(() => {
-        setShow(false);
-        alertElRef?.current?.addEventListener('transitionend', () => {
-            handleUnmount();
-        })
-    }, [handleUnmount])
+  const dismissAlert = useCallback(() => {
+    setShow(false);
+    alertElRef?.current?.addEventListener("transitionend", () => {
+      handleUnmount();
+    });
+  }, [handleUnmount]);
 
-    useEffect(() => {
-        closeBtnRef.current.focus();
-        setShow(true);
-        setTimeout(() => dismissAlert(), 4000);
-    },[dismissAlert])
+  useEffect(() => {
+    closeBtnRef.current.focus();
+    setShow(true);
+    setTimeout(() => dismissAlert(), 4000);
+  }, [dismissAlert]);
 
-    return (
-        <div ref={alertElRef} className={`sentiment ${show ? 'active' : ''}`} aria-live="polite" id="sentimentMessage">
-            <div className="sentiment__wrapper">
-                <p className="sentiment__message">{message}</p>
-                <button ref={closeBtnRef} onClick={dismissAlert} type="button"><CloseIcon></CloseIcon><span className='sr-only'>Close alert message</span></button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      ref={alertElRef}
+      className={`sentiment ${show ? "active" : ""}`}
+      aria-live="polite"
+      id="sentimentMessage"
+    >
+      <div className="sentiment__wrapper">
+        <p className="sentiment__message">{message}</p>
+        <button ref={closeBtnRef} onClick={dismissAlert} type="button">
+          <CloseIcon></CloseIcon>
+          <span className="sr-only">Close alert message</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default AlertMessage;
--- frontend/src/components/trackExercise.js
+++ frontend/src/components/trackExercise.js
@@ -1,137 +1,162 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom/client';
-import {Button, Form} from 'react-bootstrap';
-import {trackExercise, getSentimentMessage} from '../api';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './alert_message.css';
-import IconButton from '@material-ui/core/IconButton';
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
-import BikeIcon from '@material-ui/icons/DirectionsBike';
-import PoolIcon from '@material-ui/icons/Pool';
-import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
-import OtherIcon from '@material-ui/icons/HelpOutline';
-import DatePicker from 'react-datepicker';
-import AlertMessage from './alert_message';
-import 'react-datepicker/dist/react-datepicker.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Button, Form } from "react-bootstrap";
+import { trackExercise, getSentimentMessage } from "../api";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./alert_message.css";
+import IconButton from "@material-ui/core/IconButton";
+import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
+import BikeIcon from "@material-ui/icons/DirectionsBike";
+import PoolIcon from "@material-ui/icons/Pool";
+import FitnessCenterIcon from "@material-ui/icons/FitnessCenter";
+import OtherIcon from "@material-ui/icons/HelpOutline";
+import DatePicker from "react-datepicker";
+import AlertMessage from "./alert_message";
+import "react-datepicker/dist/react-datepicker.css";
 
+const TrackExercise = ({ currentUser }) => {
+  const [state, setState] = useState({
+    exerciseType: "",
+    description: "",
+    duration: 0,
+    date: new Date(),
+  });
+  const [message, setMessage] = useState("");
 
-const TrackExercise = ({currentUser}) => {
-    const [state, setState] = useState({
-        exerciseType: '',
-        description: '',
+  const handleTrackExercise = async (e) => {
+    e.preventDefault();
+
+    const dataToSubmit = {
+      username: currentUser,
+      ...state,
+    };
+
+    try {
+      const response = await trackExercise(dataToSubmit);
+      console.log(response.data);
+      showDescriptionAlert(state.description);
+
+      setState({
+        exerciseType: "",
+        description: "",
         duration: 0,
         date: new Date(),
-    });
-    const [message, setMessage] = useState('');
+      });
 
-    const handleTrackExercise = async (e) => {
-        e.preventDefault();
+      setMessage("Activity logged successfully! Well done!");
+      setTimeout(() => setMessage(""), 5000);
+    } catch (error) {
+      console.error("There was an error logging your activity!", error);
+    }
+  };
 
-        const dataToSubmit = {
-            username: currentUser,
-            ...state,
-        };
+  const closeAlert = (mount, elementHasFocus) => {
+    mount.unmount();
+    elementHasFocus.focus();
+  };
 
-        try {
-            const response = await trackExercise(dataToSubmit);
-            console.log(response.data);
-            showDescriptionAlert(state.description);
+  const showDescriptionAlert = async () => {
+    if (state.description) {
+      const message = await getSentimentMessage(state.description);
+      if (message) {
+        const alertRoot = ReactDOM.createRoot(
+          document.getElementById("alertMessage")
+        );
+        const elementHasFocus = document.activeElement;
+        alertRoot.render(
+          <AlertMessage
+            message={message}
+            handleUnmount={() => closeAlert(alertRoot, elementHasFocus)}
+          />
+        );
+      }
+    }
+  };
 
-            setState({
-                exerciseType: '',
-                description: '',
-                duration: 0,
-                date: new Date(),
-            });
-
-            setMessage('Activity logged successfully! Well done!');
-            setTimeout(() => setMessage(''), 5000);
-
-        } catch (error) {
-            console.error('There was an error logging your activity!', error);
-        }
-    };
-
-    const closeAlert = (mount, elementHasFocus) => {
-        mount.unmount();
-        elementHasFocus.focus();
-    } 
-
-    const showDescriptionAlert = async () => {
-        if (state.description) {
-            const message = await getSentimentMessage(state.description);
-            if (message) {
-                const alertRoot = ReactDOM.createRoot(document.getElementById('alertMessage'));
-                const elementHasFocus = document.activeElement;
-                alertRoot.render(
-                    <AlertMessage message={message} handleUnmount={() => closeAlert(alertRoot, elementHasFocus)} />
-                )
-            }
-        }
-    };
-
-    return (
-        <div>
-            <h3>Track exercise</h3>
-            <Form onSubmit={handleTrackExercise} style={{maxWidth: '400px', margin: 'auto'}}>
-                <Form.Group controlId="formDate" className="form-margin">
-                    <Form.Label>Date:</Form.Label>
-                    <DatePicker
-                        selected={state.date}
-                        onChange={(date) => setState({...state, date})}
-                        dateFormat="yyyy/MM/dd"
-                    />
-                </Form.Group>
-                <div style={{marginBottom: '20px'}}>
-                    <IconButton id="runningButton" color={state.exerciseType === 'Running' ? "primary" : "default"}
-                                onClick={() => setState({...state, exerciseType: 'Running'})}>
-                        <DirectionsRunIcon fontSize="large"/>
-                    </IconButton>
-                    <IconButton id="cyclingButton" color={state.exerciseType === 'Cycling' ? "primary" : "default"}
-                                onClick={() => setState({...state, exerciseType: 'Cycling'})}>
-                        <BikeIcon fontSize="large"/>
-                    </IconButton>
-                    <IconButton id="swimmingButton" color={state.exerciseType === 'Swimming' ? "primary" : "default"}
-                                onClick={() => setState({...state, exerciseType: 'Swimming'})}>
-                        <PoolIcon fontSize="large"/>
-                    </IconButton>
-                    <IconButton id="gymButton" color={state.exerciseType === 'Gym' ? "primary" : "default"}
-                                onClick={() => setState({...state, exerciseType: 'Gym'})}>
-                        <FitnessCenterIcon fontSize="large"/>
-                    </IconButton>
-                    <IconButton id="otherButton" color={state.exerciseType === 'Other' ? "primary" : "default"}
-                                onClick={() => setState({...state, exerciseType: 'Other'})}>
-                        <OtherIcon fontSize="large"/>
-                    </IconButton>
-                </div>
-                <Form.Group controlId="description" style={{marginBottom: '20px'}}>
-                    <Form.Label>Description:</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        rows={3}
-                        required
-                        value={state.description}
-                        onChange={(e) => setState({...state, description: e.target.value})}
-                    />
-                </Form.Group>
-                <Form.Group controlId="duration" style={{marginBottom: '40px'}}>
-                    <Form.Label>Duration (in minutes):</Form.Label>
-                    <Form.Control
-                        type="number"
-                        required
-                        value={state.duration}
-                        min={1}
-                        onChange={(e) => setState({...state, duration: e.target.value})}
-                    />
-                </Form.Group>
-                <Button variant="success" type="submit">
-                    Save activity
-                </Button>
-            </Form>
-            <p className={`${message ? 'fade-in' : ''} response-message`}>{message}</p>
-            <div id="alertMessage"></div>
+  return (
+    <div>
+      <h3>Track exercise</h3>
+      <Form
+        onSubmit={handleTrackExercise}
+        style={{ maxWidth: "400px", margin: "auto" }}
+      >
+        <Form.Group controlId="formDate" className="form-margin">
+          <Form.Label>Date:</Form.Label>
+          <DatePicker
+            selected={state.date}
+            onChange={(date) => setState({ ...state, date })}
+            dateFormat="yyyy/MM/dd"
+          />
+        </Form.Group>
+        <div style={{ marginBottom: "20px" }}>
+          <IconButton
+            id="runningButton"
+            color={state.exerciseType === "Running" ? "primary" : "default"}
+            onClick={() => setState({ ...state, exerciseType: "Running" })}
+          >
+            <DirectionsRunIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            id="cyclingButton"
+            color={state.exerciseType === "Cycling" ? "primary" : "default"}
+            onClick={() => setState({ ...state, exerciseType: "Cycling" })}
+          >
+            <BikeIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            id="swimmingButton"
+            color={state.exerciseType === "Swimming" ? "primary" : "default"}
+            onClick={() => setState({ ...state, exerciseType: "Swimming" })}
+          >
+            <PoolIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            id="gymButton"
+            color={state.exerciseType === "Gym" ? "primary" : "default"}
+            onClick={() => setState({ ...state, exerciseType: "Gym" })}
+          >
+            <FitnessCenterIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            id="otherButton"
+            color={state.exerciseType === "Other" ? "primary" : "default"}
+            onClick={() => setState({ ...state, exerciseType: "Other" })}
+          >
+            <OtherIcon fontSize="large" />
+          </IconButton>
         </div>
-    );
+        <Form.Group controlId="description" style={{ marginBottom: "20px" }}>
+          <Form.Label>Description:</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            required
+            value={state.description}
+            onChange={(e) =>
+              setState({ ...state, description: e.target.value })
+            }
+          />
+        </Form.Group>
+        <Form.Group controlId="duration" style={{ marginBottom: "40px" }}>
+          <Form.Label>Duration (in minutes):</Form.Label>
+          <Form.Control
+            type="number"
+            required
+            value={state.duration}
+            min={1}
+            onChange={(e) => setState({ ...state, duration: e.target.value })}
+          />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          Save activity
+        </Button>
+      </Form>
+      <p className={`${message ? "fade-in" : ""} response-message`}>
+        {message}
+      </p>
+      <div id="alertMessage"></div>
+    </div>
+  );
 };
 
 export default TrackExercise;
