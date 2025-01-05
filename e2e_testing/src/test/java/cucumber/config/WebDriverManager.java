81530a61af4ee1be1d487677d38c4cae72052b3b
AdelaideBaron <baronadelaide@hotmail.com>
2025-01-05 18:16:17 +0000

    reset web driver manager

--- e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
+++ e2e_testing/src/test/java/cucumber/config/WebDriverManager.java
@@ -16,12 +16,12 @@ public class WebDriverManager {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
-            // Remove the headless argument to make the browser visible
-            // options.addArguments("--headless"); // Comment or remove this line
-//            options.addArguments("--disable-gpu");
-//            options.addArguments("window-size=1920,1080");
 
-//            System.setProperty("DISPLAY", ":99"); // If you're on a headless server, keep this line, but otherwise, you can remove it
+             options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("window-size=1920,1080");
+
+            System.setProperty("DISPLAY", ":99");
             driver = new ChromeDriver(options);
 
             driver.get(Context.BASE_URL);
