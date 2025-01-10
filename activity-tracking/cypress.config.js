7d652334b84518040a89320455d4470bd60f9e4c
julia-anne <julia.robinson@spareroom.co.uk>
2025-01-10 19:07:05 +0000

    move support file, set value to false

--- activity-tracking/cypress.config.js
+++ activity-tracking/cypress.config.js
@@ -3,9 +3,8 @@
 
   module.exports = defineConfig({
     e2e: {
-      setupNodeEvents(on, config) {
-        // implement node event listeners here
-      },
+      baseUrl: "http://localhost",
+      supportFile: false,
     },
   });
 }
