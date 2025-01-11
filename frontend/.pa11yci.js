91efc60582f4f7174539857ac7128930f343ff56
julia-anne <julia.robinson@spareroom.co.uk>
2025-01-11 17:25:31 +0000

    pa11yci attempt

--- frontend/.pa11yci.js
+++ frontend/.pa11yci.js
@@ -3,27 +3,40 @@ module.exports = {
     concurrency: 5,
     standard: "WCAG2AA",
     runners: ["axe"],
-    timout: 90000,
   },
   urls: [
-    { url: "http://localhost:3000/login" },
+    { url: "http://localhost/login" },
+    { url: "http://localhost/signup" },
+    // {
+    //   actions: [
+    //     "click element p.mt-3 a",
+    //     "set field #formBasicEmail to juliartheuser10",
+    //     "set field #formBasicPassword to password",
+    //     "click element button[type=submit]",
+    //     "wait for url to be http://localhost/trackExercise",
+    //   ],
+    //   url: "http://localhost/trackExercise",
+    // },
     {
       actions: [
-        "set field #formUsername to newuser",
-        "set field #formPassword to password",
-        "click element button[type=submit]",
+        "wait for url to be http://localhost/signup",
+        "set field #formBasicEmail to juliartheuser19",
+        "set field #formBasicPassword to password",
         "wait for url to be http://localhost/trackExercise",
-      ],
-      url: "http://localhost/trackExercise",
-    },
-    {
-      actions: [
-        "set field #formUsername to newuser",
-        "set field #formPassword to password",
-        "click element button[type=submit]",
+        "screen capture example.png",
+        "click element .nav-link:nth-of-type(3)",
         "wait for url to be http://localhost/journal",
       ],
       url: "http://localhost/journal",
     },
+    // {
+    //   actions: [
+    //     "set field #formUsername to newuser15",
+    //     "set field #formPassword to password",
+    //     "click element button[type=submit]",
+    //     "wait for url to be http://localhost/journal",
+    //   ],
+    //   url: "http://localhost/journal",
+    // },
   ],
 };
