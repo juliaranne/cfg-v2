e78baf2ba76adae7fa68ce5b27f63ecec74a631b
AdelaideBaron <baronadelaide@hotmail.com>
2025-01-12 17:58:40 +0000

    testing the view all page actions

--- e2e_testing/src/test/java/cucumber/stepDefs/ViewAllSteps.java
+++ e2e_testing/src/test/java/cucumber/stepDefs/ViewAllSteps.java
@@ -4,14 +4,16 @@ import cucumber.util.Context;
 import cucumber.webHelpers.WebActions;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.Assertions;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 public class ViewAllSteps {
 
     private WebActions webActions = Context.get(Context.KEY_WEB_ACTIONS, WebActions.class);
@@ -22,16 +24,34 @@ public class ViewAllSteps {
         String expectedType = Context.get(Context.KEY_EXERCISE_TYPE).toString();
         String expectedDescription = Context.get(Context.KEY_EXERCISE_DESCRIPTION).toString();
 
+        WebElement myExercise = getSingleExercise();
+        log.info("addi");
+        log.info(myExercise.getText());
+        String myExerciseInfo = myExercise.getText();
+        assertTrue(myExerciseInfo.contains(expectedDuration));
+    }
+
+    @When("I select the delete button")
+    public void iSelectTheDeleteButton() {
+        WebElement myExercise = getSingleExercise();
+        WebElement deleteButton = webActions.findElementByCssSelector("button.close-button");
+        deleteButton.click();
+    }
+
+    @Then("the exercise is no longer visible on the View All page")
+    public void theExerciseIsNoLongerVisibleOnTheViewAllPage() {
+        WebElement exercisesList = webActions.findElementByCssSelector("ul[data-testid='viewAllList']");
+
+        List<WebElement> exerciseItems = exercisesList.findElements(By.cssSelector("li.exercise-view-all-data"));
+        assertEquals(0, exerciseItems.size());
+    }
+
+    private WebElement getSingleExercise(){
         WebElement exercisesList = webActions.findElementByCssSelector("ul[data-testid='viewAllList']");
 
         List<WebElement> exerciseItems = exercisesList.findElements(By.cssSelector("li.exercise-view-all-data"));
         assertEquals(1, exerciseItems.size());
-        WebElement myExercise = exerciseItems.getFirst();
-        String exerciseType = myExercise.findElement(By.tagName("strong")).getText();
-        String duration = myExercise.getText().split("\n")[1];
-        String description = myExercise.getText().split("\n")[2];
-        assertEquals(expectedDescription, description);
-        assertEquals(expectedDuration, duration);
-        assertEquals(expectedType, exerciseType);
+
+        return exerciseItems.getFirst();
     }
 }
--- e2e_testing/src/test/resources/cucumber/ViewAll.feature
+++ e2e_testing/src/test/resources/cucumber/ViewAll.feature
@@ -11,4 +11,9 @@ Feature: View All page
     When I click on "View All"
     Then I can see the details of my workout
 
-#  Scenario: Deleting exercise from View All page removes it everywhere
+  Scenario: Deleting exercise from View All page removes it everywhere
+    Given I click on "View All"
+    When I select the delete button
+    Then the exercise is no longer visible on the View All page
+    And I click on "Statistics"
+    Then there is no data available
