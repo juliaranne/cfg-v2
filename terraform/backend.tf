78ce3dcab77fda23accc887dc51df19f0387b5ea
Olivia <olivia@maxwellnet.com>
2025-01-13 21:48:47 +0000

    tf fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,9 @@ terraform {
   backend "s3" {
     # The name of the S3 bucket where the Terraform state will be stored
     bucket = "mla-fitness-app-tf-state"
-    
+
+    key = "env:/dev/dev/terraform.tfstate"
+
     # The AWS region where the S3 bucket is located
     region = "eu-west-2"
   }
